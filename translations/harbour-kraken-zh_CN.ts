7135d8aab11de270a184f9d559b8f7cc790f2874
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2020-01-14 00:32:37 +0800

    Create harbour-kraken-zh_CN.ts

---
+++ translations/harbour-kraken-zh_CN.ts
@@ -0,0 +1,94 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>Balance</name>
+    <message>
+        <source>Needs login</source>
+        <translation>需要登录</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>登录</translation>
+    </message>
+    <message>
+        <source>Balance</source>
+        <translation>资产负债表</translation>
+    </message>
+</context>
+<context>
+    <name>Credentials</name>
+    <message>
+        <source>Credentials</source>
+        <translation>凭证</translation>
+    </message>
+    <message>
+        <source>API key</source>
+        <translation>API 密钥</translation>
+    </message>
+    <message>
+        <source>Enter API key</source>
+        <translation>输入 API 密钥</translation>
+    </message>
+    <message>
+        <source>API secret</source>
+        <translation>API 密码</translation>
+    </message>
+    <message>
+        <source>Save credentials</source>
+        <translation>保存凭证</translation>
+    </message>
+    <message>
+        <source>Enter API secret</source>
+        <translation>输入 API 密码</translation>
+    </message>
+</context>
+<context>
+    <name>Home</name>
+    <message>
+        <source>Update credentials</source>
+        <translation>更新凭证</translation>
+    </message>
+    <message>
+        <source>Add credentials</source>
+        <translation>添加凭证</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>刷新</translation>
+    </message>
+    <message>
+        <source>Market</source>
+        <translation>市场</translation>
+    </message>
+    <message>
+        <source>Balance</source>
+        <translation>资产负债表</translation>
+    </message>
+</context>
+<context>
+    <name>Market</name>
+    <message>
+        <source>Currency</source>
+        <translation>货币</translation>
+    </message>
+</context>
+<context>
+    <name>PairDetails</name>
+    <message>
+        <source>Refresh now</source>
+        <translation>立即刷新</translation>
+    </message>
+</context>
+<context>
+    <name>PairLabel</name>
+    <message>
+        <source>Day high</source>
+        <translation>当日最高价</translation>
+    </message>
+    <message>
+        <source>Day low</source>
+        <translation>当日最低价</translation>
+    </message>
+</context>
+</TS>
