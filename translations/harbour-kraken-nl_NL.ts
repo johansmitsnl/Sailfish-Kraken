b468388f6450b16d87101c4301a72c8ca417ec27
Johan Smits <johan@smitsmail.net>
2020-01-13 19:26:53 +0100

    Update project

--- translations/harbour-kraken-nl_NL.ts
+++ translations/harbour-kraken-nl_NL.ts
@@ -4,17 +4,17 @@
 <context>
     <name>Balance</name>
     <message>
-        <location filename="../qml/views/Balance.qml" line="27"/>
+        <location filename="../qml/views/Balance.qml" line="20"/>
         <source>Needs login</source>
         <translation>Behoeft inlog</translation>
     </message>
     <message>
-        <location filename="../qml/views/Balance.qml" line="32"/>
+        <location filename="../qml/views/Balance.qml" line="25"/>
         <source>Login</source>
         <translation>Login</translation>
     </message>
     <message>
-        <location filename="../qml/views/Balance.qml" line="45"/>
+        <location filename="../qml/views/Balance.qml" line="38"/>
         <source>Balance</source>
         <translation>Balans</translation>
     </message>
@@ -22,32 +22,32 @@
 <context>
     <name>Credentials</name>
     <message>
-        <location filename="../qml/pages/Credentials.qml" line="29"/>
+        <location filename="../qml/pages/Credentials.qml" line="26"/>
         <source>Credentials</source>
         <translation>Credentials</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Credentials.qml" line="33"/>
+        <location filename="../qml/pages/Credentials.qml" line="30"/>
         <source>API key</source>
         <translation>API sleutel</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Credentials.qml" line="40"/>
+        <location filename="../qml/pages/Credentials.qml" line="37"/>
         <source>Enter API key</source>
         <translation>Vul API sleutel in</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Credentials.qml" line="48"/>
+        <location filename="../qml/pages/Credentials.qml" line="45"/>
         <source>API secret</source>
         <translation>API geheim</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Credentials.qml" line="56"/>
+        <location filename="../qml/pages/Credentials.qml" line="53"/>
         <source>Enter API secret</source>
         <translation>Vul API geheim in</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Credentials.qml" line="65"/>
+        <location filename="../qml/pages/Credentials.qml" line="62"/>
         <source>Save credentials</source>
         <translation>Sla credentials op</translation>
     </message>
@@ -55,27 +55,27 @@
 <context>
     <name>Home</name>
     <message>
-        <location filename="../qml/pages/Home.qml" line="153"/>
+        <location filename="../qml/pages/Home.qml" line="26"/>
         <source>Update credentials</source>
         <translation>Credentials</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Home.qml" line="153"/>
+        <location filename="../qml/pages/Home.qml" line="26"/>
         <source>Add credentials</source>
         <translation>Credentials toevoegen</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Home.qml" line="158"/>
+        <location filename="../qml/pages/Home.qml" line="31"/>
         <source>Refresh</source>
         <translation>Bijwerken</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Home.qml" line="179"/>
+        <location filename="../qml/pages/Home.qml" line="52"/>
         <source>Market</source>
         <translation>Markt</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Home.qml" line="184"/>
+        <location filename="../qml/pages/Home.qml" line="57"/>
         <source>Balance</source>
         <translation>Balans</translation>
     </message>
@@ -91,7 +91,7 @@
 <context>
     <name>PairDetails</name>
     <message>
-        <location filename="../qml/pages/PairDetails.qml" line="81"/>
+        <location filename="../qml/pages/PairDetails.qml" line="70"/>
         <source>Refresh now</source>
         <translation>Nu bijwerken</translation>
     </message>
@@ -99,12 +99,12 @@
 <context>
     <name>PairLabel</name>
     <message>
-        <location filename="../qml/views/PairLabel.qml" line="81"/>
+        <location filename="../qml/views/PairLabel.qml" line="77"/>
         <source>Day high</source>
         <translation>Vandaag hoog</translation>
     </message>
     <message>
-        <location filename="../qml/views/PairLabel.qml" line="92"/>
+        <location filename="../qml/views/PairLabel.qml" line="88"/>
         <source>Day low</source>
         <translation>Vandaag laag</translation>
     </message>
